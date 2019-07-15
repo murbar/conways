97d9287d6259c0b93393ce6b3aa5a48f6e499759
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-15 17:01:56 -0400

    Add rules

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Conway's Game of Life</header>
+      <div className="rules">
+        <h2>Rules</h2>
+        <ul>
+          <li>If a cell is alive and has exactly 2 or 3 neighbors it stays alive</li>
+          <li>If a cell is alive and it have less than 2 or more than 4 neighbors it dies</li>
+          <li>If a cell is dead and it has exactly 3 live neighbors it comes to life</li>
+        </ul>
     </div>
   );
 }
