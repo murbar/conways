2636309be7ab6ec75ec80cf49763fe76f8feee04
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-15 17:02:45 -0400

    Setup basic grid state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Grid from './Grid';
+
+const gridSize = 30;
+
+const initGrid = (size, random = false) =>
+  Array(size)
+    .fill()
+    .map(() =>
+      Array(size)
+        .fill()
+        .map(() => {
+          return random ? Math.round(Math.random()) : 0;
+        })
+    );
+
+const AppWrapper = styled.div`
+  margin: 1rem 2rem 3rem;
+`;
 
 function App() {
+  const [gameState, setGameState] = useState(initGrid(gridSize, true));
+
+  const randomize = () => {
+    const newState = initGrid(gridSize, true);
+    setGameState(newState);
+  };
+
   return (
+    <AppWrapper>
+      <header className="App-header">
+        <h1>Conway's Game of Life</h1>
+      </header>
+
+      <button onClick={randomize}>Randomize</button>
+      <Grid state={gameState} />
+
       <div className="rules">
         <h2>Rules</h2>
         <ul>
@@ -9,7 +43,8 @@ function App() {
           <li>If a cell is alive and it have less than 2 or more than 4 neighbors it dies</li>
           <li>If a cell is dead and it has exactly 3 live neighbors it comes to life</li>
         </ul>
-    </div>
+      </div>
+    </AppWrapper>
   );
 }
 
