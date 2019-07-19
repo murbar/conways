2f413c8babde821666c416034044eb4b0722f9c3
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-18 21:02:51 -0400

    Add content

--- src/components/About.js
+++ src/components/About.js
@@ -3,22 +3,15 @@ import styled from 'styled-components';
 
 const Styles = styled.div``;
 
-const Blockquote = styled.blockquote`
-  font-size: 0.9em;
-  border-left: 0.1em solid grey;
-  margin: 0 2rem;
-  padding: 0 2rem;
-`;
-
 export default function About() {
   return (
     <Styles>
       <h2>What is it?</h2>
       <p>
-        Invented by Cambridge mathematician John Conway, the Game of Life is a game with no players.
-        More simulation than game, cells on a grid live and die according to a simple set of rules.
-        A cell has eight neighboring cells and it's next state is determined by the number of those
-        cells that are alive. Upon each iteration of the grid:
+        Invented by Cambridge mathematician John Conway in 1970, the Game of Life is a "game" with
+        no players. More simulation than game, cells on a grid live and die according to a simple
+        set of rules. A cell has eight neighboring cells and it's next state is determined by the
+        number of those cells that are alive. Upon each iteration of the grid:
       </p>
       <ul>
         <li>A living cell with less than 2 neighbors will die from loneliness</li>
@@ -26,25 +19,23 @@ export default function About() {
         <li>A living cell with 2 or 3 neighbors is contented and lives on</li>
         <li>An empty cell with exactly 3 neighbors will spontaneously come to life</li>
       </ul>
-      <p>s</p>
       <p>
-        From <a href="http://www.conwaylife.com/wiki/Conway%27s_Game_of_Life">LifeWiki</a>:
+        Conway's game was inspired by work done in the 1940s by John von Neumann who was searching
+        for a hypothetical machine that could self-replicate. The <em>Game of Life</em> was the
+        product of Conway's efforts to simplify von Neumann's ideas.
       </p>
-      <Blockquote>
-        Conway was interested in a problem presented in the 1940s by renowned mathematician John von
-        Neumann, who tried to find a hypothetical machine that could build copies of itself ... The
-        Game of Life emerged as Conway's successful attempt to simplify von Neumann's ideas. ...
-        From a theoretical point of view, it is interesting because it has the power of a universal
-        Turing machine: that is, anything that can be computed algorithmically can be computed
-        within Conway's Game of Life.
-      </Blockquote>
-      <h2>Cellular Automata</h2>
       <p>
-        What is is, applications in computer science, turing completeness... Lorem ipsum dolor, sit
-        amet consectetur adipisicing elit. At, eveniet eius, laudantium expedita est consequatur
-        voluptatem rem natus accusamus quas ab, eligendi quo. Aspernatur aliquam ea quo, debitis
-        dolor sed?
+        The work is of interest to computer scientists because it is possible to build the
+        elementary parts of a computer, like memory and logic gates, inside the simulation. This
+        makes <em>Life</em> a universal Turing machine &mdash; theoretically as powerful as any
+        computer with unlimited time and memory and capable of computing anything that can be
+        computed algorithmically.
       </p>
+      <p>
+        The game's concepts of emergent complexity and self-organization are influential in a wide
+        range of fields including physics, biology, math, economics, philosophy and art.{' '}
+      </p>
+
       <h2>Technical Considerations</h2>
 
       <p>
