81a6025bfb71c3b56b81c364b08dfbc496a9335f
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-19 15:12:18 -0400

    Add content

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import { ReactComponent as PlayIcon } from '../icons/play.svg';
 import { ReactComponent as PauseIcon } from '../icons/pause.svg';
 import { ReactComponent as StepIcon } from '../icons/forward.svg';
 import { ReactComponent as RandomizeIcon } from '../icons/shuffle.svg';
-import { ReactComponent as ResetIcon } from '../icons/x.svg';
+import { ReactComponent as ResetIcon } from '../icons/grid.svg';
 import { media } from '../styles/helpers';
 
 const Styles = styled.div`
@@ -18,7 +18,7 @@ const Styles = styled.div`
 `;
 
 const Instructions = styled.div`
-  background: ${p => p.theme.colors.secondary};
+  background: rgba(0, 0, 0, 0.5);
   padding: 1rem 1.5rem;
   border-radius: 1rem;
   font-size: 0.8em;
@@ -111,15 +111,157 @@ export default function About() {
         The game's concepts of emergent complexity and self-organization are influential in a wide
         range of fields including physics, biology, math, economics, philosophy and art.{' '}
       </p>
+      <h2>Big Ideas</h2>
+      <p>
+        I’d encountered Conway’s game years ago after I read{' '}
+        <a href="https://www.amazon.com/Emergence-Connected-Brains-Cities-Software/dp/0684868768">
+          <em>Emergence</em>
+        </a>{' '}
+        by Steven Johnson. The ideas in that book led me to Wolfram’s{' '}
+        <a href="https://www.amazon.com/gp/product/1579550088">
+          <em>A New Kind of Science</em>
+        </a>
+        , which I found completely impenetrable, but further led me to the field of cellular
+        automata, where I found <em>The Game of Life</em>.
+      </p>
+      <p>
+        At the time I was fascinated with the idea of enormously complex systems emerging from a
+        simple set of rules and constraints. Ruminating on these these concepts, I wondered: could
+        the entire universe, with its extraordinary complexity, have evolved from just a relatively
+        few laws of physics? Do we exist in a giant simulation, albeit one that is the size of the
+        universe and has been running for 14 billion years?
+      </p>
+      <p>
+        What distinguishes this simulation from “reality”, or is there any meaningful difference at
+        all? And what about free will; if every moment in time is determined by the state of the
+        universe in the moment before it and will evolve according to a set of fixed rules, is the
+        future not already set?
+      </p>
+      <p>
+        These ideas still interest me today so I was excited to build my own simple version Conway’s
+        game.
+      </p>
+      <h2>My Process</h2>
+      <p>
+        Polya's famous method for solving mathematical problems is widely applicable to other fields
+        of study, especially computer science and algorithm design. He outlines four steps to follow
+        when approaching a problem:
+      </p>
+      <ul>
+        <li>take time to fully understand the problem,</li>
+        <li>devise a plan,</li>
+        <li>implement the plan,</li>
+        <li>reflect upon, verify, and possibly improve the solution.</li>
+      </ul>
 
-      {/* <h2>Technical Considerations</h2>
+      <h3>Understanding the problem</h3>
 
       <p>
-        Polya process, React, double-buffers, canvas, possible improvments... Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Ab sapiente excepturi adipisci fuga animi cumque
-        veritatis error rerum quis, laboriosam dolore delectus porro dolorem rem enim obcaecati
-        ipsum distinctio aspernatur.
-      </p> */}
+        First, I knew that I wanted to build this project for the web so that it was accessible to
+        the widest possible audience. If I’d built it with Python for example, the user would have
+        to download some code and figure out how to run it locally. My library of choice is React
+        when building UIs for the web.
+      </p>
+
+      <p>
+        After some quick research on the web and taking a look at other examples of the game, I knew
+        we’d need components for a grid, cells on the grid, and controls for the simulation. We’d
+        also have to maintain the state of the grid and have functions that could manipulate the
+        state as well as transform the state between iterations.
+      </p>
+
+      <p>
+        Would using plain React components to represent cells be sufficiently performant? What if
+        our grid has thousands of cells? I decided to render each cell as a styled <code>div</code>{' '}
+        and I’d explore other possibilities if and when it became a performance bottleneck. Get it
+        working first, optimize later as needed.
+      </p>
+
+      <h3>Planning</h3>
+
+      <p>
+        A 2d array of 1s and 0s (alive or empty) seemed like a good choice for cells on the grid.
+        During each iteration we loop through all the cells in the grid, count the immediate
+        neighbors of each cell, and determine the cell’s next state based on the rules of the game.
+      </p>
+
+      <p>
+        Evolving the grid from one state to the next is a pure function—we return a new array based
+        on the previous grid’s state instead of mutating the array in place. This is important
+        because if we change the state of the grid while we're iterating over it we won’t get the
+        results we’re after. We’re in luck since in React state is immutable, this concept of a
+        “double buffer” is built in. We grab the application state, build a new state array based on
+        it, and set our application state to the new array.
+      </p>
+
+      <p>
+        Evolving the grid from one state to the next would be implemented using{' '}
+        <code>setInterval</code>, probably without too much effort in React with hooks.
+      </p>
+
+      <p>
+        We’d need a way to toggle cells on the grid as well, in a way that was comfortable and
+        obvious for the user. And there should be a display for the current generation and
+        population count.
+      </p>
+      <h3>Implementation</h3>
+
+      <p>
+        Within a few hours, I had the app working with about 80% of features implemented. It turns
+        out that React is really good at rendering a lot of components while making only the minimum
+        required updates to the DOM. Especially after build optimizations, using a <code>div</code>{' '}
+        for each cell performed perfectly fine even with 2500+ cells.
+      </p>
+
+      <p>
+        Using React hooks, I was able to painlessly incorporate <code>setInterval</code> in the app
+        and set the interval to a value saved in the game’s configuration state.
+      </p>
+
+      <p>
+        It took the better part of a day to work out the logic and combination of event listeners to
+        get the cell toggle functionality to work the way I envisioned. I wanted the user to not
+        only be able to click and toggle a cell's state, but also able to click and drag to “draw”
+        cells on the grid in a natural way. After some trial and error, I was able to achieve the
+        desired outcome.
+      </p>
+
+      <p>
+        I spent some time creating the speed toggle component from scratch. I could have searched
+        for a 3rd party library to accomplish the same functionality, but I find that it takes time
+        to locate the right solution and even when found, it’s often more complex than necessary. I
+        value simplicity in the code that I write, or at least only as much complexity as necessary
+        to solve the problem and no more. I ended up with exactly what I wanted in surprisingly
+        little time, further testament to the power of React.
+      </p>
+
+      <p>
+        Ensuring that the game was mobile-first and also took full advantage of a wide-screen
+        desktop display was not as trivial as I expected, though it was time well spent. I had to
+        brush up on some CSS techniques to maintain the square aspect ratio of the grid at all
+        times.
+      </p>
+
+      <p>
+        To jazz it up a little, I added keyboard shortcuts for the controls and spent some extra
+        time getting the styling just right. We know that the user’s first impression is important;
+        if we don’t get the UI right, no one will want to use it, no matter how elegant or clever
+        the code under the hood.
+      </p>
+
+      <h3>Retrospective</h3>
+
+      <p>
+        Looking back, I would like to explore re-writing the grid and cells with the{' '}
+        <code>canvas</code> API. While the app works just fine now, the performance would be
+        improved significantly and we would be able to play with a much larger grid. I could explore
+        a full-screen mode with a pannable grid as well.
+      </p>
+
+      <p>
+        I enjoyed building this project, learned a few things along the way, and hope you have some
+        fun tinkering in the Game of Life!
+      </p>
     </Styles>
   );
 }
