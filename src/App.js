529b73ef84b2094dfbd03e7d7132f9cb4ea73970
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-20 17:56:03 -0400

    Include Preset

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { media } from './styles/helpers';
-import { initGrid, stepGrid, countPopulation } from './lifeLogic';
+import Header from './components/Header';
 import GridDisplay from './components/GridDisplay';
-import useInterval from './useInterval';
-import useHotKeys from './useHotKeys';
-import Footer from './components/Footer';
 import Controls from './components/Controls';
 import Stats from './components/Stats';
 import About from './components/About';
-import Header from './components/Header';
+import Presets from './components/Presets';
+import Footer from './components/Footer';
+import { media } from './styles/helpers';
+import { initGrid, stepGrid, countPopulation } from './lifeLogic';
+import useInterval from './useInterval';
+import useHotKeys from './useHotKeys';
 
 const AppWrapper = styled.div`
   padding: 0 2rem 3rem;
@@ -100,14 +101,14 @@ function App() {
     if (!(gridPreset.length <= config.gridSize)) {
       console.error('Grid preset too large for current grid');
     } else {
-      // duplicate before setting
-      setGridState(gridPreset.map(row => [...row]));
+      setGridState(() => {
+        // if (!isPaused) playPause();
+        // duplicate before setting
+        setEvolutionInterval(null);
+        return gridPreset.map(row => [...row]);
+      });
     }
   };
-
-  useInterval(() => {
-    step();
-  }, evolutionInterval);
 
   const logStatePretty = () => {
     // pretty print grid state for debug & easy copy/paste
@@ -116,6 +117,10 @@ function App() {
     log += ']';
     console.log(log);
   };
+
+  useInterval(() => {
+    step();
+  }, evolutionInterval);
 
   useHotKeys({
     p: playPause,
@@ -129,14 +134,11 @@ function App() {
     <AppWrapper>
       <Header />
       <GameContainer>
-        <Controls
-          isPaused={isPaused}
-          callbacks={{ playPause, step, randomize, reset, setSpeed, loadPreset }}
-        />
+        <Controls isPaused={isPaused} callbacks={{ playPause, step, randomize, reset, setSpeed }} />
         <GridDisplay state={gridState} setCell={setCell} isPaused={isPaused} />
         <Stats genCount={genCount} popCount={popCount} isPaused={isPaused} />
       </GameContainer>
-
+      <Presets loadPreset={loadPreset} />
       <About />
       <Footer />
     </AppWrapper>
