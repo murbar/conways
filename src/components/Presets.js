ba54f7b01c0b155e27ba315376f0c5273217ac2d
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-23 12:10:38 -0400

    Refactor presets UI

--- src/components/Presets.js
+++ src/components/Presets.js
@@ -7,80 +7,84 @@ import { ReactComponent as GunPreview } from '../images/gun-preset.svg';
 import { ReactComponent as OscilPreview } from '../images/oscill-preset.svg';
 import { gliderGun, gliderArray, oscillators, spaceships } from '../presetGrids';
 
-const ButtonGroup = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-`;
-
-const PresetButton = styled(Button)`
-  display: block;
-  margin: 0 0 1rem 0;
-  padding: 1.5rem;
-  text-align: left;
-  min-height: 12rem;
+const Preset = styled.div`
+  margin-bottom: 3rem;
   svg {
-    width: 9rem;
-    height: 9rem;
+    width: 8rem;
+    height: 8rem;
     position: absolute;
+    color: ${p => p.theme.colors.primary};
   }
   .title {
     font-family: ${p => p.theme.fontFamily};
-    display: block;
     font-size: 1.25em;
-    margin-bottom: 0.5rem;
-    margin-left: 10.5rem;
+    margin-bottom: 0.75rem;
+    margin-left: 9.5rem;
+    line-height: 1;
   }
   .description {
-    display: block;
-    margin-left: 10.5rem;
+    margin-left: 9.5rem;
     font-family: ${p => p.theme.fontFamily};
     font-size: 0.9em;
     line-height: 1.5;
+  }
+  .load {
+    margin: 1rem 0 0 9.5rem;
+    ${'' /* text-align: right; */}
   }
 `;
 
 export default function Presets({ loadPreset }) {
   return (
-    <div>
+    <>
       <h2>Interesting patterns</h2>
-      <ButtonGroup>
-        <PresetButton onClick={() => loadPreset(spaceships)}>
-          <ShipsPreview />
-          <span className="title">Spaceships</span>
-          <span className="description">
-            These patterns cycle over a fixed number of generations while traveling across the grid.
-            New patterns are being discovered all the time. Three of the smallest{' '}
-            <em>elementary</em> ships are demonstrated here.
-          </span>
-        </PresetButton>
-        <PresetButton onClick={() => loadPreset(gliderArray)}>
-          <GlidersPreview />
-          <span className="title">Glider Array</span>
-          <span className="description">
-            First discovered in 1969, the glider is the smallest and most common <em>spaceship</em>.
-            It travels diagonally over a period of 4 generations and can be used to transmit
-            information over large distances in complex systems.
-          </span>
-        </PresetButton>
-        <PresetButton onClick={() => loadPreset(gliderGun)}>
-          <GunPreview />
-          <span className="title">Glider Gun</span>
-          <span className="description">
-            A <em>gun</em> is a stationary pattern that continuously emits spaceships. Shown here is
-            a well-known pattern called the Gosper glider gun. Because the edges of our grid wrap
-            around to their opposite edge, this gun will eventually be destroyed by its own fire.
-          </span>
-        </PresetButton>
-        <PresetButton onClick={() => loadPreset(oscillators)}>
-          <OscilPreview />
-          <span className="title">Oscillators</span>
-          <span className="description">
-            These patterns are stationary and cycle over a fixed period. Oscillators with a period
-            of 1 appear constant and are known as <em>still life</em>; those that move around the
-            grid are <em>spaceships</em>.
-          </span>
-        </PresetButton>
-      </ButtonGroup>
-    </div>
+      <Preset>
+        <ShipsPreview />
+        <div className="title">Spaceships</div>
+        <div className="description">
+          These patterns cycle over a fixed number of generations while traveling across the grid.
+          Three of the smallest <em>elementary</em> ships are demonstrated here.
+        </div>
+        <div className="load">
+          <Button onClick={() => loadPreset(spaceships)}>Load Spaceships</Button>
+        </div>
+      </Preset>
+      <Preset>
+        <GlidersPreview />
+        <div className="title">Glider Array</div>
+        <div className="description">
+          First discovered in 1969, the glider is the smallest and most common <em>spaceship</em>.
+          It travels diagonally over a period of 4 generations and can be used to transmit
+          information over large distances in complex systems.
+        </div>
+        <div className="load">
+          <Button onClick={() => loadPreset(gliderArray)}>Load Glider Array</Button>
+        </div>
+      </Preset>
+      <Preset>
+        <GunPreview />
+        <div className="title">Glider Gun</div>
+        <div className="description">
+          A <em>gun</em> is a stationary pattern that continuously emits spaceships. Shown here is a
+          well-known pattern called the Gosper glider gun. Because the edges of our grid wrap around
+          to their opposite edge, this gun will eventually be destroyed by its own fire.
+        </div>
+        <div className="load">
+          <Button onClick={() => loadPreset(gliderGun)}>Load Glider Gun</Button>
+        </div>
+      </Preset>
+      <Preset>
+        <OscilPreview />
+        <div className="title">Oscillators</div>
+        <div className="description">
+          These patterns are stationary and cycle over a fixed period. Oscillators with a period of
+          1 appear constant and are known as <em>still life</em>; those that move around the grid
+          are <em>spaceships</em>.
+        </div>
+        <div className="load">
+          <Button onClick={() => loadPreset(oscillators)}>Load Oscillators</Button>
+        </div>
+      </Preset>
+    </>
   );
 }
