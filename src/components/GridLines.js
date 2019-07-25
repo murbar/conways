3ed801751378182b65d492c4a6c441c59efa4a4f
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-24 20:17:54 -0400

    Tidy up

--- src/components/GridLines.js
+++ src/components/GridLines.js
@@ -3,42 +3,46 @@ import { withTheme } from 'styled-components';
 
 const dpr = window.devicePixelRatio || 1;
 
-function GridLines({ theme, dimension }) {
-  const ref = useRef();
+function GridLines({ theme, gridSize }) {
+  const canvasRef = useRef();
 
   useEffect(() => {
-    const c = ref.current;
-    const ctx = c.getContext('2d');
-    const gridRect = c.getBoundingClientRect();
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const gridRect = canvas.getBoundingClientRect();
 
-    c.width = gridRect.width * dpr;
-    c.height = gridRect.width * dpr;
+    canvas.width = gridRect.width * dpr;
+    canvas.height = gridRect.width * dpr;
     ctx.scale(dpr, dpr);
 
-    const cellSize = c.width / dpr / dimension;
-    const gridSize = c.width / dpr;
+    const cellSize = canvas.width / dpr / gridSize;
+    const gridWidth = canvas.width / dpr;
 
     ctx.strokeStyle = theme.colors.secondary;
     ctx.lineWidth = 1;
-    ctx.setLineDash([1, 1]);
 
-    ctx.strokeRect(0, 0, gridSize, gridSize);
+    ctx.strokeRect(0, 0, gridWidth, gridWidth);
 
-    for (let linePos = cellSize; linePos < gridSize - cellSize; linePos += cellSize) {
+    const boundary = gridWidth - Math.floor(cellSize);
+    for (let linePos = cellSize; linePos < boundary; linePos += cellSize) {
       // rows
       ctx.beginPath();
       ctx.moveTo(0, linePos);
-      ctx.lineTo(gridSize, linePos);
+      ctx.lineTo(gridWidth, linePos);
       ctx.stroke();
       // cols
       ctx.beginPath();
       ctx.moveTo(linePos, 0);
-      ctx.lineTo(linePos, gridSize);
+      ctx.lineTo(linePos, gridWidth);
       ctx.stroke();
     }
-  }, [theme, dimension]);
+  }, [theme, gridSize]);
 
-  return <canvas ref={ref} />;
+  return (
+    <div>
+      <canvas id="grid-canvas" ref={canvasRef} />
+    </div>
+  );
 }
 
 export default withTheme(GridLines);
