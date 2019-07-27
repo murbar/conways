c8e9ef2c9d48cc2dbb20599a9ddc7e1fea07a011
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-27 10:09:21 -0400

    Add theme colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,17 +4,25 @@ const colors = {
   cream: 'hsl(39, 77%, 95%)',
   blueBlack: 'hsl(210, 14%, 11%)',
   blueGrey: 'hsl(214, 16%, 28%)',
-  blue: 'hsl(196, 100%, 50%)'
+  blue: 'hsl(196, 100%, 50%)',
+  red: 'hsl(1, 90%, 54%)',
+  gold: 'hsl(44, 100%, 48%)'
 };
+
+const addAlpha = (hsl, alpha) => {
+  return `${hsl.slice(0, -1)}, ${alpha})`;
+};
+
+const primary = colors.blue;
 
 const theme = {
   colors: {
     ...colors,
     background: colors.blueBlack,
     foreground: colors.cream,
-    primary: colors.blue,
+    primary: primary,
     secondary: colors.blueGrey,
-    cellShadowGradientStart: 'hsla(196, 100%, 50%, 0.2)'
+    cellShadowGradientStart: addAlpha(primary, 0.2)
   },
   inputBorderRadius: `0.5rem`,
   fontFamily: "'Rubik', sans-serif",
